14ce9d595db7faa8738dfbffc6ee026f940dd998
Brayan1951 <brayanc1951@gmail.com>
2024-01-16 14:51:19 -0500

    update

--- src/db/getExcel.js
+++ src/db/getExcel.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export const temp_excel=async(datos)=>{
-    const response = await axios.post('http://127.0.0.1:8000/excel/generar_excel',datos,{
+
+    const baseref='https://cotizador-example-1.onrender.com'
+    console.log(datos);
+    // const baseref='http://127.0.0.1:8000'
+    const response = await axios.post(`${baseref}/excel/generar_excel`,datos,{
         responseType: 'arraybuffer',  // Para manejar archivos binarios (Excel)
     })
     const blob = new Blob([response.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
